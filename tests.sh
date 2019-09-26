ee760422f2033f6d2687638ecb8cc47158f47fa3
Harris Hoke <hhoke@fas.harvard.edu>
2019-09-26 13:30:15 -0400

    different return codes and --parseable option

--- tests.sh
+++ tests.sh
@@ -21,13 +21,29 @@ function test_suite {
   chmod 000 $noPermsDir
 
   ExistsFile=$(mktemp)
+  noExistsDir=$testdir/notDir
+  noExistsFile=$testdir/first/notFile
+  
+  
   #Note for later: inconsistent camelCase and python_style
   # test fpc  
   test_returnval "fpc $deepdir" 0
-  test_returnval "fpc $noPermsFile" 1
-  test_returnval "fpc $lostDir" 1
-  test_error_message "fpc $noPermsFile" "path exists with -x permissions up to '$(dirname $noPermsFile)'\n==========| '$noPermsFile' does not have permission -r"
-  test_error_message "fpc $lostDir" "path exists with -x permissions up to '$(dirname $lostDir)'\n==========| '$noPermsFile' does not have permission -r"
+  test_returnval "fpc $noPermsFile" 77
+  test_returnval "fpc $lostDir" 77
+  test_returnval "fpc $noExistsDir" 66
+  test_returnval "fpc $noExistsFile" 66
+
+  test_error_message "fpc $noPermsFile" "'$noPermsFile' does not have permission -r"
+  test_error_message "fpc $lostDir" "path exists with -x permissions up to '$(dirname $noPermsDir)'
+==========| '$(basename $noPermsDir)' does not have permission -x"
+  test_error_message "fpc $noExistsDir " "path exists with -x permissions up to '$testdir'
+==========| 'notDir' does not exist"
+  test_error_message "fpc $noExistsFile " "path exists with -x permissions up to '$testdir/first'
+==========| 'notFile' does not exist"
+  test_error_message "fpc $noPermsFile --parseable" "$(dirname $noPermsFile)"
+  test_error_message "fpc $lostDir --parseable" "$testdir/first"
+  test_error_message "fpc $noExistsDir --parseable" "$testdir"
+  test_error_message "fpc $noExistsFile --parseable" "$testdir/first"
 
   # test ppc
 
@@ -42,12 +58,20 @@ function test_suite {
 function test_error_message {
   cmd=$1
   intended_error=$2
-  error_string=$($cmd)
+  error_string="$($cmd 2>&1)"
   returnval=$?
-  if [ "$returnval" != "$target_returnval" ];then
-    echo "failed error for $cmd: $error instead of $intended_error"
+  if [ "$intended_error" != "$error_string" ];then
+    echo
+    echo :::
+    echo "failed error for $cmd:"
+    echo "[$error_string]" instead of 
+    echo "[$intended_error]"
     _fpc_err=1
+  else
+    echo
+    echo 'PASSED'
   fi 
+
 }
 
 function test_returnval {
@@ -56,8 +80,12 @@ function test_returnval {
   $cmd
   returnval=$?
   if [ $returnval != $target_returnval ];then
+    echo
+    echo :::
     echo "failed return value for $cmd: $returnval instead of $target_returnval"
     _fpc_err=1
+  else
+    echo 'PASSED'
   fi 
 }
 
